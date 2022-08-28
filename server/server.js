9f7cb6e267bb7f4598d38f14932dd7ba154cc5cb
Nick Earl <nick.r.earl@gmail.com>
2022-08-28 13:05:29 -0400

    added auth with jwt. added routes for thoughts reactions and friends

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
-// import our typeDefs and resolvers
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -10,7 +8,8 @@ const PORT = process.env.PORT || 3001;
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
